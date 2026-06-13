6d41066c38305ce3d5a36bae23b0641867777405
ilgabri <cigabri@live.it>
2026-06-13 14:52:22 +0200

    added examples

---
+++ examples/make_ConvexHull_from_database_and_ML/input_IPSE_CH.py
@@ -0,0 +1,55 @@
+#here all defaults variables for the IPSE_CH 'module' of IPSE-DISCO are stored
+
+#CH general options
+#data_types_CH=["mongo","ML","file"] #list, can have entries: "mongo","ML","file"
+data_types_CH=["file","ML"] #list, can have entries: "mongo","ML",and/or "file"
+print_CH=True
+plot_CH=False
+save_CH=True
+CH_elements=["Pb","S","Cl"]
+elemental_energies= {}
+input_totalE=False
+print_unstable=True
+threshold_instability=0.05
+threshold_instability_plot=0.07
+exclude_polymorphs=True
+
+#ML options
+ML_model_file='ML_model.pkl'
+ML_scaler_file='scaler.pkl'
+compounds4ML=[] #to evaluate specific compounds. Example: ["Sn1Sb5S9I1","Sn1Sb3S9I1"]
+
+max_elements_composition=10 #either one number of a list as long as the numberof elements of the convex hull or none
+use_features_subset=False #whether to use only a subset of features (defined by features_subset_file, typically "impo_feats.txt") in ML for CH
+features_subset_file="impo_feats.txt"
+include_ML_in_CH=True #if False, the stability of compounds evlauated with ML is evaluated but the compounds are not used to build the CH
+charge_neutral_only=True #check for charge neutrality (it has to be in features!) and exclude non-charge neutrals from ML predictions
+
+ncpus_features=1
+
+
+#file options
+compounds_files=["oqmd_chalcohal_rhof.csv","bin2_oqmd.csv"] #file(s) where compounds and Eform are read from, can be csv or text (can be a list of files!)
+file_has_header=True
+column_compound=1
+column_E=2
+column_label = None
+include_file_compounds_in_CH=True #whether compounds read from file are used to build the CH (similar to include_ML_in_CH)
+
+#mongo options
+db_server_name="mongodb://localhost:27017/"
+db_name=None
+db_collection_name=None
+include_ID=False
+
+#additional options
+calculate_XRD=False
+stability_threshold_XRD=0.0 #the XRD is calculated for all structures within this distance from hull. None to avoid this (only structure_files_XRD) 
+structures_path="." #where to look for structures (absolute path or relative to the script). By default, assumes same folder name as label and CONTCAR
+#(add 2nd default?). None for 
+#keep in mind that at some point we might add xrd from mongoDB
+
+#this might be dict (label:path) or just list of paths, in which case, the last folder becomes the label.It must include the file name!
+structure_files_XRD=None #list of str: additional specific structures for which the xrd is calculated. Added to structures_path!
+twotheta_range=(0,90)
+
---
+++ examples/make_ConvexHull_from_database_and_ML/input_IPSE_ML.py
@@ -0,0 +1,52 @@
+styles_ML_features=["matminer","ID2","ID3","ID4"] 
+options_ML_styles={
+    "ID1":['atomic_radius_rahm','atomic_volume','covalent_radius_bragg','covalent_radius_cordero',
+        'dipole_polarizability','e_affinity','en_allen','electronegativity_martynov-batsanov',
+        'en_pauling','electronegativity_sanderson','en_ghosh','ionization_energy',
+        'mendeleev_number','metallic_radius','vdw_radius_alvarez','vdw_radius_mm3'],
+    "ID2":[],
+    "ID3":['periods','groups','entropy'],
+    "ID4":[],
+    "matminer":"magpie",
+}
+
+
+skipDB=True 
+#anions=["F","Cl","Br","I","O","S","Se","Te","As"]
+anions=["Cl","Br","I","S","Se","Te"]
+cations=       ["Li","Na","K","Rb","Cs"]
+cations.extend(["Be","Mg","Ca","Sr","Ba"])
+cations.extend(["Sc","Ti","V" ,"Cr","Mn","Fe","Co","Ni","Cu","Zn"])
+cations.extend(["Y" ,"Zr","Nb","Mo","Tc","Ru","Rh","Pd","Ag","Cd"])
+cations.extend(["La","Hf","Ta","W" ,"Re","Os","Ir","Pt","Au","Hg"])
+cations.extend(["Al","Ga","In","Tl"])
+cations.extend(["Sn","Pb","Sb","Bi"])
+
+
+file_name_data_fitting= "oqmd_chalcohal_rhof"
+
+
+validation=False
+fraction_validation=None
+
+n_test_train_splits=10
+
+ncpus_features=4
+ncpus_ML_algo=20
+ncpus_feat_importance=1 
+ncpus_cv_regularization_combo=1
+ncpus_cv_traintest=1
+
+do_features_analysis=False
+dropcolumn_feature_importance=True #cpu expensive but meaningful
+features_combinator_operators = None #allowed values: "polynomial","+","-","*","/","^2","^3","^-1","exp". None or empty to do feature importance w/o feat combination
+regularized_model_featcombo="elasticnet" # allowd options: elasticnet, lasso, elasticnetCV, lassoCV, SVR
+file_important_feats="impo_feats" #ifile on which to save/read labels of important features; if ...; None to avoid saving/reading important features
+validate_with_impo_feats=False
+
+
+shapley_feature_importance=False
+try_features_combination=False
+threshold_important_features= None
+ML_algorithms=["PICK_RF_ne600_mf0.5_bsFalse"]
+
